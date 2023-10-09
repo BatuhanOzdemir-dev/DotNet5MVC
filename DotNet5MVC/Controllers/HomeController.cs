2dedaed97cb8fe3c745d11acbabd941a0879cba3
Batuhan Özdemir <batuhan22.ozdemir@gmail.com>
2023-10-09 23:31:43 +0300

    Add project files.

---
+++ DotNet5MVC/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet5MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
